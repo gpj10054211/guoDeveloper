c9f553c0d1cf3eec9c0b5e64b46742262aee01dc
guopengju <guopengju@meituan.com>
2023-09-07 21:28:48 +0800

    dynamic_program

---
+++ dynamic_program.py
@@ -0,0 +1,42 @@
+import sys,os,re
+import copy,heapq
+from collections import deque
+from typing import Optional, List
+
+class DynamicProgram:
+    def __init__(self):
+        self.data = []
+
+    #回文子串：给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。回文字符串 是正着读和倒过来读一样的字符串。
+    #子字符串 是字符串中的由连续字符组成的一个序列。
+    #具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+    #输入：s = "abc"
+    #输出：3
+    #解释：三个回文子串: "a", "b", "c"
+    def countSubstrings(self, s: str) -> int:
+        return 0
+
+
+
+if __name__ == '__main__':
+    DP = DynamicProgram()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
